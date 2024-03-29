c7430235f94ecac5d29220910c91cb383004acb8
shnmj <shnmje@gmail.com>
2024-03-29 19:56:49 +0900

    BoardMapper.xml ~ (수정중)

---
+++ src/main/java/com/board/mapper/BoardMapper.java
@@ -0,0 +1,31 @@
+package com.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.board.domain.BoardVo;
+import com.board.menus.domain.MenuVo;
+
+@Mapper
+public interface BoardMapper {
+
+	List<BoardVo> getBoardList(MenuVo menuVo);
+
+	void insertBoard(BoardVo boardVo);
+
+	BoardVo getBoard(BoardVo boardVo);
+
+	void incHit(BoardVo boardVo);
+
+	void deleteBoard(BoardVo boardVo);
+
+	void updateBoard(BoardVo boardVo);
+
+}
+
+
+
+
+
+
